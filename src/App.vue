b1f0663c81761bad3d5367b2b3ad8aa981f1b127
Ari Yuan <wiseway@163.con>
2020-10-09 21:19:04 +0800

    vue kanban commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,115 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container mt-5">
+    <div class="row">
+      <div class="col form-inline">
+        <b-form-input
+          v-model="newTask"
+          placeholder="Enter Task"
+          @keyup.enter="add"
+        ></b-form-input>
+        <b-button class="ml-2" variant="primary" @click="add">Add</b-button>
+      </div>
+    </div>
+    <div class="row mt-3">
+      <div class="col-md-3">
+        <div class="p-2 alert alert-primary">
+          <h3>Backlog</h3>
+          <draggable class="list-group kanban-column" :list="arrBacklog" group="tasks">
+            <div
+              class="list-group-item"
+              v-for="ele in arrBacklog"
+              :key="ele.name"
+            >
+              {{ ele.name }}
+            </div>
+          </draggable>
+        </div>
+      </div>
+
+      <div class="col-md-3">
+        <div class="p-2 alert alert-secondary">
+          <h3>In Progress</h3>
+          <draggable class="list-group kanban-column" :list="arrInProgress" group="tasks">
+            <div
+              class="list-group-item"
+              v-for="ele in arrInProgress"
+              :key="ele.name"
+            >
+              {{ ele.name }}
+            </div>
+          </draggable>
+        </div>
+      </div>
+
+      <div class="col-md-3">
+        <div class="p-2 alert alert-warning">
+          <h3>Backlog</h3>
+          <draggable class="list-group kanban-column" :list="arrTested" group="tasks">
+            <div
+              class="list-group-item"
+              v-for="ele in arrTested"
+              :key="ele.name"
+            >
+              {{ ele.name }}
+            </div>
+          </draggable>
+        </div>
+      </div>
+
+      <div class="col-md-3">
+        <div class="p-2 alert alert-success">
+          <h3>Backlog</h3>
+          <draggable class="list-group kanban-column" :list="arrDone" group="tasks">
+            <div
+              class="list-group-item"
+              v-for="ele in arrDone"
+              :key="ele.name"
+            >
+              {{ ele.name }}
+            </div>
+          </draggable>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import draggable from "vuedraggable";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    draggable,
+  },
+  data() {
+    return {
+      newTask: "",
+      arrBacklog: [
+        { name: "Code Sign Up page" },
+        { name: "Test DashBoard" },
+        { name: "style Reg" },
+        { name: "Help with Designs" },
+      ],
+      arrInProgress: [],
+      arrTested: [],
+      arrDone: [],
+    };
+  },
+  methods: {
+    add() {
+      if (this.newTask) {
+        this.arrBacklog.push({ name: this.newTask });
+        this.newTask = "";
+      }
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.kanban-column {
+  min-height: 300px;
 }
+
 </style>
