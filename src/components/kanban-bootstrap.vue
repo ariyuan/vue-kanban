6326d3ee00eb1aeec7980f5ccfc4e98b2bb1df8d
Ari Yuan <wiseway@163.con>
2020-10-12 21:23:35 +0800

    Refactor component part

--- src/components/kanban-bootstrap.vue
+++ src/components/kanban-bootstrap.vue
@@ -1,134 +1,58 @@
 <template>
   <div class="container mt-5">
     <div class="row mt-3">
-      <div class="col-md-3">
-        <div class="p-2 alert alert-primary">
-          <h3>Backlog</h3>
-          <draggable
-            class="list-group kanban-column"
-            :list="arrBacklog"
-            group="tasks"
-          >
-            <div
-              class="list-group-item"
-              v-for="ele in arrBacklog"
-              :key="ele.name"
-            >
-              {{ ele.name }}
-            </div>
-          </draggable>
-          <a
-            href=""
-            @click.prevent="onAddingNewBlock = !onAddingNewBlock"
-            v-if="!onAddingNewBlock"
-            >+ Add new block</a
-          >
-          <b-form-input
-          v-model="newTask"
-          placeholder="Enter Task"
-          @keyup.enter="add"
-          v-if="onAddingNewBlock"
-          @blur="onInputLostfocus()"
-        ></b-form-input>
-        </div>
-      </div>
-
-      <div class="col-md-3">
-        <div class="p-2 alert alert-secondary">
-          <h3>In Progress</h3>
-          <draggable
-            class="list-group kanban-column"
-            :list="arrInProgress"
-            group="tasks"
-          >
-            <div
-              class="list-group-item"
-              v-for="ele in arrInProgress"
-              :key="ele.name"
-            >
-              {{ ele.name }}
-            </div>
-          </draggable>
-        </div>
-      </div>
-
-      <div class="col-md-3">
-        <div class="p-2 alert alert-warning">
-          <h3>Tested</h3>
-          <draggable
-            class="list-group kanban-column"
-            :list="arrTested"
-            group="tasks"
-          >
-            <div
-              class="list-group-item"
-              v-for="ele in arrTested"
-              :key="ele.name"
-            >
-              {{ ele.name }}
-            </div>
-          </draggable>
-        </div>
-      </div>
-
-      <div class="col-md-3">
-        <div class="p-2 alert alert-success">
-          <h3>Done</h3>
-          <draggable
-            class="list-group kanban-column"
-            :list="arrDone"
-            group="tasks"
-          >
-            <div class="list-group-item" v-for="ele in arrDone" :key="ele.name">
-              {{ ele.name }}
-            </div>
-          </draggable>
-        </div>
-      </div>
+      <kanbanColumn
+        v-for="item in columns"
+        :key="item.column_name"
+        :arrList="item.content"
+        :showAddButton="item.showAddButton"
+        :columnclass="item.columnclass"
+      ></kanbanColumn>
     </div>
   </div>
 </template>
 
 <script>
-import draggable from "vuedraggable";
+import kanbanColumn from "./kanban-column";
 
 export default {
   components: {
-    draggable,
+    kanbanColumn,
   },
   data() {
     return {
-      onAddingNewBlock: false,
-      newTask: "",
-      arrBacklog: [
-        { name: "Code Sign Up page" },
-        { name: "Test DashBoard" },
-        { name: "style Reg" },
-        { name: "Help with Designs" },
+      columns: [
+        {
+          column_name: "Backlog",
+          content: [
+            { name: "Code Sign Up page" },
+            { name: "Test DashBoard" },
+            { name: "style Reg" },
+            { name: "Help with Designs1" },
+          ],
+          showAddButton: true,
+          columnclass: "alert-primary",
+        },
+        {
+          column_name: "In Progress",
+          content: [],
+          showAddButton: false,
+          columnclass: "alert-secondary",
+        },
+        {
+          column_name: "Tested",
+          content: [],
+          showAddButton: false,
+          columnclass: "alert-warning",
+        },
+        {
+          column_name: "Done",
+          content: [],
+          showAddButton: false,
+          columnclass: "alert-success",
+        },
       ],
-      arrInProgress: [],
-      arrTested: [],
-      arrDone: [],
     };
-  },
-  methods: {
-    onInputLostfocus(){
-      this.add();
-      this.onAddingNewBlock = false;
-    },
-    add() {
-      if (this.newTask) {
-        this.arrBacklog.push({ name: this.newTask });
-        this.newTask = "";
-        this.onAddingNewBlock = false;
-      }
-    },
   },
 };
 </script>
-
-<style scoped>
-.kanban-column {
-  min-height: 300px;
-}
-</style>
