df550b3e31f279360db70735770d2485159ce5b5
Ari Yuan <wiseway@163.con>
2020-10-13 20:39:00 +0800

    Make column draggable

--- src/components/kanban-bootstrap.vue
+++ src/components/kanban-bootstrap.vue
@@ -1,24 +1,26 @@
 <template>
-  <div class="container mt-5">
-    <div class="row mt-3">
-      <kanbanColumn
-        v-for="item in columns"
-        :key="item.column_name"
-        :arrList="item.content"
-        :column_name="item.column_name"
-        :showAddButton="item.showAddButton"
-        :columnclass="item.columnclass"
-      ></kanbanColumn>
-    </div>
+  <div class="container-fluid mt-5">
+      <draggable class="row mt-3" :list="columns" group="tasks">
+        <kanbanColumn
+          v-for="item in columns"
+          :key="item.column_name"
+          :arrList="item.content"
+          :column_name="item.column_name"
+          :showAddButton="item.showAddButton"
+          :columnclass="item.columnclass"
+        ></kanbanColumn>
+      </draggable>
   </div>
 </template>
 
 <script>
 import kanbanColumn from "./kanban-column";
+import draggable from "vuedraggable";
 
 export default {
   components: {
     kanbanColumn,
+    draggable,
   },
   data() {
     return {
