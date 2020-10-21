16a2150f20990c8697ab5a502d19e142f3cf050f
Ari Yuan <wiseway@163.con>
2020-10-21 20:56:51 +0800

    Make card detail prototype

--- src/components/card-detail.vue
+++ src/components/card-detail.vue
@@ -2,8 +2,22 @@
   <div>
     <a href="#" @click="$bvModal.show(card_id)" class="float-right">...</a>
 
-    <b-modal :id="card_id" title="Card Detail">
-      <p class="my-4">Description placeholder</p>
+    <b-modal :id="card_id" title="Card Detail" size="lg">
+      <div class="container row">
+        <div class="col-9">
+          <span>Description:</span>
+          <b-form-textarea
+            id="textarea"
+            v-model="text"
+            placeholder="Description placeholder"
+            rows="3"
+            max-rows="6"
+          ></b-form-textarea>
+        </div>
+        <div class="col-3">
+          <span>Assignee</span>
+        </div>
+      </div>
     </b-modal>
   </div>
 </template>
